f076781f894b527e7410a88e25abd0421085811e
Luis Tesorero <luistesorero5@gmail.com>
2021-06-04 19:53:21 -0500

    test

--- src/Views/Home/index.js
+++ src/Views/Home/index.js
@@ -1,9 +1,9 @@
 import React, { useEffect } from 'react';
 import Logo from '../../Assets/images/logo.png';
 import Bg from '../../Assets/images/bg.jpg';
-import transfers from '../../Assets/images/transfers.png';
-import holders from '../../Assets/images/holders.png';
-import supply from '../../Assets/images/supply.png';
+// import transfers from '../../Assets/images/transfers.png';
+// import holders from '../../Assets/images/holders.png';
+// import supply from '../../Assets/images/supply.png';
 import { connect } from 'react-redux';
 function Home(props) {
     const { locale } = props;
