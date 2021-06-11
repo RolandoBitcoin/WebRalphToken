7380c03bcb2e4d45111da0455a00b1f927fb3896
Luis Tesorero <luistesorero5@gmail.com>
2021-06-11 11:14:29 -0500

    add carrot paper

--- src/Views/WhitePaper/index.js
+++ src/Views/WhitePaper/index.js
@@ -1,12 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 import Bg from '../../Assets/images/bg.jpg';
 // import PDFViewer from 'pdf-viewer-reactjs'
 import PDFViewer from 'mgr-pdf-viewer-react';
-// import { Document, Page } from 'react-pdf';
+import { Document, Page, pdfjs } from 'react-pdf';
+
 // https://files-ralph.s3.us-east-2.amazonaws.com/Ralph(ESP).pdf
+
+import esp from '../../Assets/whitepaperesp.pdf';
+import eng from '../../Assets/whitepapereng.pdf';
+
+const options = {
+    cMapUrl: 'cmaps/',
+    cMapPacked: true,
+};
+
 function WhitePaper(props) {
     const { locale } = props;
+    const [numPages, setNumPages] = useState(null);
+    const [pageNumber, setPageNumber] = useState(1);
+    useEffect(() => {
+        pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+    }, [])
+    function onDocumentLoadSuccess({ numPages }) {
+        setNumPages(numPages);
+        setPageNumber(1);
+    }
+
+    function changePage(offset) {
+        setPageNumber(prevPageNumber => prevPageNumber + offset);
+    }
+
+    function previousPage() {
+        changePage(-1);
+    }
+
+    function nextPage() {
+        changePage(1);
+    }
     return (
         locale && <div class="section mcb-section" id="new" style={{ paddingTop: 90, paddingBottom: 50 }}
             data-parallax="3d"><img class="mfn-parallax"
@@ -18,13 +49,39 @@ function WhitePaper(props) {
                     <div class="mcb-wrap-inner text-center">
                         <h2 style={{ color: "#e7d600" }}>CARROT PAPER</h2>
                         <div class="mcb-column one-third column_column">
-                            <PDFViewer document={{
+                            {/* <PDFViewer document={{
                                 url: locale.locale.whitePaper,
                             }}
                                 // scale={3}
                                 scaleStep={11}
                                 canvasCss="canvasStyle"
-                            />
+                            /> */}
+                            <Document
+                                file={locale.locale.in8 === "Español" ? esp : eng}
+                                onLoadSuccess={onDocumentLoadSuccess}
+                                options={options}
+                            >
+                                <Page pageNumber={pageNumber} />
+                            </Document>
+                            <div>
+                                <p>
+                                    Page {pageNumber || (numPages ? 1 : '--')} of {numPages || '--'}
+                                </p>
+                                <button
+                                    type="button"
+                                    disabled={pageNumber <= 1}
+                                    onClick={previousPage}
+                                >
+                                    Previous
+        </button>
+                                <button
+                                    type="button"
+                                    disabled={pageNumber >= numPages}
+                                    onClick={nextPage}
+                                >
+                                    Next
+        </button>
+                            </div>
                         </div>
                     </div>
                 </div>
