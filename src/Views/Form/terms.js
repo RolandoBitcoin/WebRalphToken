dd1a7052e1979e6876c74106a1cb862ad58c1ffb
Luis Tesorero <luistesorero5@gmail.com>
2021-06-21 12:57:21 -0500

    add round #13

--- src/Views/Form/terms.js
+++ src/Views/Form/terms.js
@@ -7,8 +7,8 @@ import { formatMoney } from '../../Helpers';
 function Terms(props) {
     const { locale, background } = props;
     let history = useHistory();
-    const [costPerUSD] = useState(245000000)
-    const [round] = useState(12)
+    const [costPerUSD] = useState(240000000)
+    const [round] = useState(13)
     let back = e => {
         e.stopPropagation();
         history.goBack();
